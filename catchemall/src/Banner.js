c19c1a587b32211334c81eab4698e5491c6c95a1
GrigorisPs <uranus.p@gmail.com>
2023-05-30 11:48:02 +0300

    start from scratch

--- catchemall/src/Banner.js
+++ catchemall/src/Banner.js
@@ -1,9 +1,10 @@
 export function Banner() {
-  return (
-    <>
-      <div className='d-flex justify-content-center'>
-        <img className='banner' src='https://www.freepnglogos.com/uploads/pokemon-logo-transparent-png-2.png' />
-      </div>
-    </>
-  )
-}
+    return (
+      <>
+        <div className='d-flex justify-content-center'>
+          <img className='banner' src='https://www.freepnglogos.com/uploads/pokemon-logo-transparent-png-2.png' />
+        </div>
+      </>
+    )
+  }
+  
--- catchemall/src/Game.js
+++ catchemall/src/Game.js
@@ -1,67 +1,32 @@
-import { CardContainer } from "./CardContainer";
 import { Banner } from "./banner";
-import { HorizontalLine } from "./HorizontalLine";
-import { Footer } from "./Footer";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Card } from "./Card";
+import  axios from "axios";
 
-const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
-function generateNumbers() {
-    const randomNumbers = [];
-    while (randomNumbers.length < 5) {
-        const newNumber = Math.floor(Math.random() * 151 + 1);
-        if (randomNumbers.indexOf(newNumber) === -1) {
-            randomNumbers.push(newNumber);
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/' ;
+
+const pokemonDisplayed=[];
+
+async function callPokemon(i) {
+     await axios.get(baseUrl+i).then(req=>{
+        const pokemon = {
+            name:req.data.name,
+            no:req.data.id,
+            types:req.data.types.map(type=>type.type.name)
         }
-    }
-    return randomNumbers;
+        pokemonDisplayed.push(pokemon)
+    })
 }
+callPokemon(1);
+console.log(pokemonDisplayed,'πινακας');
+console.log(pokemonDisplayed[0]);
 
-export async function getPokemon() {
-    const pokemonUrls = [];
-    const numbers = generateNumbers();
-    for (let i = 0; i < 5; i++) {
-        pokemonUrls.push(`${baseUrl}${numbers[i]}`);
-    }
-    return await Promise.all(pokemonUrls.map(async (url) => fetch(url).then(r => r.json().then(pokemon => {
-        return {
-            name: pokemon.name,
-            no: pokemon.id,
-            types: pokemon.types.map(type => type.type.name),
-            imgUrl: pokemon.sprites.front_default
-        }
-    }))))
-}
+export function Game(){
 
-
-
-
-const pokemonArray = await getPokemon();
-
-
-
-export function Game() {
-    const [pokemonCaught, setPokemonCaught] = useState([]);
-    useEffect(() => {
-        axios.get(baseUrl + 1).then(res => {
-            const pokemon = {
-                name: res.data.name,
-                no: res.data.id,
-                types: res.data.types.map(type => type.type.name),
-                imgUrl: res.data.sprites.front_default
-            }
-            setPokemonCaught(() => pokemon)
-        })
-    }, [])
-
-
-    return (
+    return(
+    <>
+        <Banner/>
         <>
-            <Banner />
-            <CardContainer pokemonArray={pokemonArray} />
-            <HorizontalLine />
-            <Footer />
+        <h1>Poke-app</h1>
         </>
+    </>
     )
 }
