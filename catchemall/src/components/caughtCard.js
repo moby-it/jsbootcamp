f3097e0d4141349cb4f02b5e10e125f550c8849c
George Spanos <gespa11019@gmail.com>
2023-06-02 19:46:25 +0300

    feat(pokecard) - can only click a poke card once

---
+++ catchemall/src/components/caughtCard.js
@@ -0,0 +1,3 @@
+export function CaughtCard({ pokemon }) {
+
+}
--- catchemall/src/components/pokeCard.js
+++ catchemall/src/components/pokeCard.js
@@ -1,14 +1,21 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { PokedexContext } from "../pokedexContext";
 
 function PokeCard({ id, name, types, imageUrl }) {
+  const [clicked, setClicked] = useState(false);
   const { catchPokemon } = useContext(PokedexContext);
+  function onCatchPokemon({ id, name, types, imageUrl }) {
+    if (!clicked) {
+      setClicked(true);
+      catchPokemon({ id, name, types, imageUrl });
+    }
+  }
   return <div className="pokecard">
     <h2>#{id}</h2>
     <h2>{name}</h2>
     <h3>{types.join(",")}</h3>
     <img src={imageUrl} alt={name} />
-    <button className="btn" onClick={() => catchPokemon({ id, name, types, imageUrl })}>Catch</button>
+    <button className="btn" onClick={() => onCatchPokemon({ id, name, types, imageUrl })}>Catch</button>
   </div>;
 }
 export default PokeCard;
