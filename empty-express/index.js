1d81e2c65a85052c0c4137bf3f821d59152b03a1
George Spanos <gespa11019@gmail.com>
2023-06-21 18:54:26 +0300

    change folder structure - start catchemall-api

--- empty-express/index.js
+++ empty-express/index.js
@@ -4,6 +4,8 @@ import path from 'path';
 
 import { fileURLToPath } from 'url';
 
+let rootCalls = 0;
+const urlsCalled = [];
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
@@ -12,15 +14,35 @@ const app = express();
 
 const port = 3000;
 
+app.use((req, res, next) => {
+  urlsCalled.push(req.url);
+  next()
+});
 
 app.get('/console.js', (req, res) => {
   res.sendFile('console.js', { root: __dirname });
 });
 
-app.get('/', (req, res) => {
-  res.sendFile('box.html', { root: __dirname });
+app.get('/', increateRootCalls, LogRootCalls, rootEndpointHandler);
+app.get("/urls", (req, res) => {
+  res.send(urlsCalled.toString());
 });
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+function increateRootCalls(req, res, next) {
+  rootCalls++;
+  next();
+}
+function LogRootCalls(req, res, next) {
+  console.log("2nd middlware running");
+  console.log("Root calls are ", rootCalls);
+  if (rootCalls === 5) {
+    res.send("Root Calls are 5");
+    return;
+  }
+  next();
+}
+function rootEndpointHandler(req, res, next) {
+  res.send("Hello from root route");
+}
