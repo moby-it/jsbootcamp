16e3471a62516da8d568966b1ddf624f7e8d57ff
George Spanos <gespa11019@gmail.com>
2023-06-23 20:15:29 +0300

    write caught pokemon to file

--- catchemall-api/pokemon.js
+++ catchemall-api/pokemon.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-
+import fs from 'fs';
 /**
  * @typedef {Object} Pokemon
  * @property {string} id - Pokemon pokedex id
@@ -15,18 +15,31 @@ import express from 'express';
 const pokemonCaught = [];
 
 export const pokedexRouter = express.Router();
-
-pokedexRouter.post("/catch", (req, res) => {
-  res.send(JSON.stringify(req.body));
+const pokemonCaughtFileName = "pokemonCaught.json";
+pokedexRouter.post("/catch", async (req, res) => {
   const pokemonId = req.body.id;
   if (pokemonId) {
     if (!pokemonCaught.map(p => p.id).includes(pokemonId)) {
       pokemonCaught.push(req.body);
+      await savePokemon(req.body);
+      res.send(JSON.stringify(req.body));
     } else {
       console.warn(`WARNING: duplicate pokemon send. ${req.body.name} is already caught.`);
+      res.sendStatus(409);
+
     }
   }
 });
 pokedexRouter.get("/caught", (req, res) => {
   res.json(pokemonCaught);
 });
+async function savePokemon(pokemon) {
+  const exists = fs.existsSync(pokemonCaughtFileName);
+  if (!exists) {
+    fs.writeFileSync(pokemonCaughtFileName, JSON.stringify([]));
+  }
+  const data = fs.readFileSync(pokemonCaughtFileName, 'utf-8');
+  const pokemonCaught = JSON.parse(data);
+  pokemonCaught.push(pokemon);
+  fs.writeFileSync(pokemonCaughtFileName, JSON.stringify(pokemonCaught));
+}
