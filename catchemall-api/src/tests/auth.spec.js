604038651052a0f5a39eaa6fe1cda3e2c91946f2
George  Spanos <gespa11019@gmail.com>
2023-08-13 20:05:49 +0300

    basic supertest auth testing

--- catchemall-api/src/tests/auth.spec.js
+++ catchemall-api/src/tests/auth.spec.js
@@ -1,5 +1,23 @@
-describe('smoke test', () => {
-  it('should pass', () => {
-   expect(5).toBeTruthy()
+import express from "express";
+import request from "supertest";
+import { registerMiddleware } from "../middleware.js";
+import { authRouter } from "../auth.js";
+
+describe('Auth Router Test', () => {
+  let app;
+  beforeAll(() => {
+    app = express();
+    registerMiddleware(app);
+    app.use("/auth", authRouter);
+  });
+  describe('register', () => {
+    it('should throw 400 if no body sent', async () => {
+      await request(app).post('/auth/register').expect(400);
+    });
+  });
+  describe('login', () => {
+    it('should return status 400 if no body sent', async () => {
+      await request(app).post('/auth/login').expect(400);
+    });
   });
 });
