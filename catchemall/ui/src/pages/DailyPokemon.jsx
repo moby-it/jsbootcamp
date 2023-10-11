f3c25aa318d6a9a0cfbbecf867eec0061d29ac5e
George Spanos <gspanos@moby-it.com>
2023-10-11 19:26:13 +0300

    feat(trade) - finalize trade modal

--- catchemall/ui/src/pages/DailyPokemon.jsx
+++ catchemall/ui/src/pages/DailyPokemon.jsx
@@ -1,16 +1,16 @@
 
 import { useContext } from 'react';
+import { CaughtPokemonPokeCard } from '../components/CatchPokemonCard';
 import { CaughtList } from '../components/CaughtList';
-import PokeCard from '../components/PokeCard';
-import { PokedexContext } from '../context/pokedexContext';
+import { PokemonContext } from '../context/pokemonContext';
 
 export function DailyPokemon() {
-  const { dailyPokemonQuery, dailyPokemon } = useContext(PokedexContext);
+  const { dailyPokemonQuery, dailyPokemon } = useContext(PokemonContext);
   if (dailyPokemonQuery.isFetching) return <h2>Loading...</h2>;
   if (dailyPokemonQuery.error) return <h2>Error</h2>;
   return <>
     <div className='pokecard-list'>
-      {dailyPokemon.map(pokemon => <PokeCard key={pokemon.id} {...pokemon} />)}
+      {dailyPokemon.map((pokemon, index) => <CaughtPokemonPokeCard key={`${pokemon.id}_${index}`} {...pokemon} />)}
     </div>
     <CaughtList />
   </>;
