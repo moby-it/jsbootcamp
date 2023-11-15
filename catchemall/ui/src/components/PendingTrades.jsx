b249423162a3f7b7689fe1f92ca1b0f01d0ae135
Armaroth <uranus.P@gmail.com>
2023-11-15 13:52:23 +0200

    fix: pending trades split to "initiated" and "awaiting response". Also fixed pending trades not rerendering on declining a trade

--- catchemall/ui/src/components/PendingTrades.jsx
+++ catchemall/ui/src/components/PendingTrades.jsx
@@ -6,6 +6,7 @@ export function PendingTrades() {
     const pendingTradesQuery = usePendingTrades();
     const updateTrade = useUpdateTrade();
     const [accepted, setAccepted] = useState(null);
+
     useEffect(() => {
         if (updateTrade.isSuccess) {
             updateTrade.reset();
@@ -17,12 +18,14 @@ export function PendingTrades() {
         }
     }, [updateTrade.isSuccess, accepted]);
 
+
     if (pendingTradesQuery.isLoading || !pendingTradesQuery.isSuccess) return <p>loading...</p>;
 
     return (
         <section className="pending-trades">
             <h2>Pending Trades</h2>
-            {pendingTradesQuery.data.map((pt) => (
+            <h3>Initiated</h3>
+            {pendingTradesQuery.data.filter(pt => pt.initiated === true).map((pt) => (
                 <article key={pt.id} className="pending-trade">
                     <section className="pokemon-to-trade" aria-label="pokemon-to-trade">
                         <p
@@ -42,24 +45,49 @@ export function PendingTrades() {
                             <img src={pt.responder_pokemon_image_url} height="50" alt={pt.responder_pokemon_name} />
                         </p>
                     </section>
-                    <div role="group" aria-label="Accept or Reject Trade">
-                        <CheckCircle
-                            role="button"
-                            aria-label="accept"
-                            onClick={() => {
-                                updateTrade.mutate({ tradeId: pt.id, accepted: true });
-                                setAccepted(true);
-                            }}
-                        />
-                        <DeleteCircle
-                            role="button"
-                            aria-label="reject"
-                            onClick={() => {
-                                updateTrade.mutate({ tradeId: pt.id, accepted: false });
-                                setAccepted(false);
-                            }}
-                        />
-                    </div>
+                </article>
+            ))}
+            <h3>Awaiting response</h3>
+            {pendingTradesQuery.data.filter(pt => pt.initiated === false).map((pt) => (
+                <article key={pt.id} className="pending-trade">
+                    <section className="pokemon-to-trade" aria-label="pokemon-to-trade">
+                        <p
+                            className="my-pokemon"
+                            data-owned={pt.initiated}
+                            title={pt.initiated ? 'owned' : 'to receive'}
+                        >
+                            {pt.initiator_pokemon_name}
+                            <img src={pt.initiator_pokemon_image_url} height="50" alt={pt.initiator_pokemon_name} />
+                        </p>
+                        <p
+                            className="responder-pokemon"
+                            data-owned={!pt.initiated}
+                            title={!pt.initiated ? 'owned' : 'to receive'}
+                        >
+                            {pt.responder_pokemon_name}
+                            <img src={pt.responder_pokemon_image_url} height="50" alt={pt.responder_pokemon_name} />
+                        </p>
+                    </section>{
+
+                        <div role="group" aria-label="Accept or Reject Trade">
+                            <CheckCircle
+                                role="button"
+                                aria-label="accept"
+                                onClick={() => {
+                                    updateTrade.mutate({ tradeId: pt.id, accepted: true });
+                                    setAccepted(true);
+                                }}
+                            />
+                            <DeleteCircle
+                                role="button"
+                                aria-label="reject"
+                                onClick={() => {
+                                    updateTrade.mutate({ tradeId: pt.id, accepted: false });
+                                    setAccepted(false);
+                                }}
+                            />
+                        </div>
+                    }
                 </article>
             ))}
         </section>
